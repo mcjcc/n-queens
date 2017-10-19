2719d165de0bedbfd280fba23e3a57006d3ae55f
mcjcc <mcjohnnychen@gmail.com>
2017-10-19 15:47:26 -0700

    rooks finished

--- src/solvers.js
+++ src/solvers.js
@@ -15,7 +15,9 @@
 var rooksToggler = function(newBoard, n) {
   for (var i = 0; i < n; i++) { // row
     for (var j = 0; j < n; j++) { // column 
-      newBoard.togglePiece(i, j);
+      if (newBoard.get(i)[j] === 0) {
+        newBoard.togglePiece(i, j);
+      }
       if (newBoard.hasColConflictAt(j)) {
         newBoard.togglePiece(i, j);
         continue;
@@ -29,8 +31,6 @@ var rooksToggler = function(newBoard, n) {
   return newBoard.rows();
 };
 
-
-
 window.findNRooksSolution = function(n) {
   newBoard = new Board({n: n});
 
@@ -42,17 +42,59 @@ window.findNRooksSolution = function(n) {
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
-  rooksArr = [];
-  for (var i = 0; i < n; i++) { // row
-    for (var j = 0; j < n; j++) { // column 
-      newBoard = new Board({n: n});
-      newBoard.togglePiece(i, j);
-      rooksArr.push(rooksToggler(newBoard));
+  var rooksArr = [];
+
+  // recursiveFunc(boardList, 0);
+  var recursiveFunc = function(boardList, currentRow) {
+    
+    if ( currentRow === n ) {
+      return boardList;
     }
+    
+    var currentBoardList = [];
+    for (var i = 0; i < boardList.length; i++) {
+      for (var j = 0; j < n; j++) {
+        var stringifiedArray = JSON.stringify(boardList[i]);
+        var parsedArray = JSON.parse(stringifiedArray);
+        var newBoard = new Board(parsedArray);
+
+        // var newBoard = new Board(boardList[i]);
+        newBoard.togglePiece(currentRow, j);
+        currentBoardList.push(newBoard);
+      } //row
+    }
+    
+    currentBoardList = currentBoardList.filter(function(element) {
+      return !element.hasAnyRooksConflicts();
+    });
+    
+    var currentBoardListArrayForm = currentBoardList.map(function(e) {
+      return e.rows();
+    });
+    
+    return recursiveFunc(currentBoardListArrayForm, currentRow + 1);
+  };
+
+  for (var i = 0; i < n; i++) {
+    var board = new Board({n: n});
+    board.togglePiece(0, i);
+    rooksArr = rooksArr.concat(recursiveFunc([board.rows()], 1));
   }
-  var solutionCount = undefined; //fixme
+  // for (var i = 0; i < n; i++) { // row
+  //   for (var j = 0; j < n; j++) { // column 
+  //     newBoard = new Board({n: n});
+  //     newBoard.togglePiece(i, j);
+  //     rooksArr.push(rooksToggler(newBoard, n));
+  //   }
+  // }
+  console.log('rooksArr: ', JSON.stringify(rooksArr));
+//   var rooksArrStringed = rooksArr.map(function(e) { return JSON.stringify(e); } );
+//   result = _.uniq(rooksArrStringed);
+//   console.log(JSON.stringify(result).split('],[').join(']\n  ['));
+  var solutionCount = rooksArr.length; //fixme
 
   console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
+
   return solutionCount;
 };
 
